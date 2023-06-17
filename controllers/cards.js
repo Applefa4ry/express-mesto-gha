c00b4f3f5debb1d9b3526af293ac136ef5c25e43
Applefa4ry <apple.f4iry@yandex.ru>
2023-06-17 22:39:37 +0300

    Update cards.js

--- controllers/cards.js
+++ controllers/cards.js
@@ -45,7 +45,6 @@ module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-    { new: true },
     {
       new: true, // обработчик then получит на вход обновлённую запись
       runValidators: true, // данные будут валидированы перед изменением
@@ -66,7 +65,6 @@ module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } }, // убрать _id из массива
-    { new: true },
     {
       new: true, // обработчик then получит на вход обновлённую запись
       runValidators: true, // данные будут валидированы перед изменением

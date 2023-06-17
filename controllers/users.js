eae460dd1134a0cbe3fd7930451868d3f2d5416b
Applefa4ry <apple.f4iry@yandex.ru>
2023-06-17 22:12:49 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -28,7 +28,7 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.json({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: err.message });

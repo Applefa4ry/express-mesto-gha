363f738d4b83d1cd411fe17d6108e4445a154afc
Applefa4ry <apple.f4iry@yandex.ru>
2023-07-01 20:31:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -37,13 +37,13 @@ app.post('/signup', celebrate({
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().required().pattern(/(http?s:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/),
     email: Joi.string().required().email(),
-    password: Joi.string().min(8),
+    password: Joi.string().required().min(8),
   }),
 }), createUser);
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().min(8),
+    password: Joi.string().required().min(8),
   }),
 }), login);
 

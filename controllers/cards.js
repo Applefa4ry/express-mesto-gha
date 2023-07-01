3aa108eca2ae9aa78aebe3a45851b11988ced778
Applefa4ry <apple.f4iry@yandex.ru>
2023-07-02 01:50:28 +0300

    Update cards.js

--- controllers/cards.js
+++ controllers/cards.js
@@ -60,7 +60,7 @@ module.exports.likeCard = (req, res, next) => {
       res.send({ data: card });
     })
     .catch((err) => {
-      if (err.name === 'Error') {
+      if (err.name === 'Error' && err.message !== 'Неверный ID карточки') {
         next(new InvalidRequest('Ошибка при лайке карточки'));
       } else {
         next(err);

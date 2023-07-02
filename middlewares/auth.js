d92d636d1a4975139ccade9591cb4942b4fddccc
Applefa4ry <apple.f4iry@yandex.ru>
2023-07-02 11:28:46 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,23 @@
 const jwt = require('jsonwebtoken');
-const AuthError = require('../errors/auth-error');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const token = req.cookie.jwt;
+  const { cookie } = req.headers;
+  if (!cookie || !cookie.startsWith('jwt=')) {
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
+  }
+
+  const token = cookie.replace('jwt=', '');
   let payload;
 
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    next(new AuthError('Необходима авторизация'));
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
   }
   req.user = payload;
 

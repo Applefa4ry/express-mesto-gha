0d257e1d395ce18bdfa01d4af684c215500f4491
Applefa4ry <apple.f4iry@yandex.ru>
2023-07-02 11:26:07 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -119,7 +119,6 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: true,
         });
       res.send({ token });
     })

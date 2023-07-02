811f3462dc4d01a89633bc48d8d4e4f5758fa976
Applefa4ry <apple.f4iry@yandex.ru>
2023-07-02 10:33:19 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -112,6 +112,7 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
+          sameSite: true,
         });
       res.send({ token });
     })
